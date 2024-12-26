c58c157d8c8396a52fc113e3c1b808bcbbbf4f70
dinesh <dindina@gmail.com>
2024-12-26 15:31:29 +1100

    ValidPalindrome

--- src/week1/ValidParentheses.java
+++ src/week1/ValidParentheses.java
@@ -3,6 +3,7 @@ package week1;
 import java.util.Stack;
 
 /*
+https://leetcode.com/problems/valid-parentheses
 Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 An input string is valid if:
