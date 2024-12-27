a48444e62211ce133580f1f467895c041b4b821c
dinesh <dindina@gmail.com>
2024-12-27 14:49:39 +1100

    BestTimeToBuyAndSellStock

--- src/week1/ValidPalindrome.java
+++ src/week1/ValidPalindrome.java
@@ -57,4 +57,16 @@ public class ValidPalindrome {
         }
         return true;
     }
+
+    public boolean isPalindromeUsingRegex(String s) {
+
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int i=0, j=s.length()-1;
+        while(i <= j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
